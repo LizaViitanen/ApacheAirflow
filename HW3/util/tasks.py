99cf84e6c2b8b4b85d03da7a29c825883e1adfc4
Liza <LizaViitanen@gmail.com>
2021-04-14 13:49:20 +0300

    добавлены xcom'ы

--- HW3/util/tasks.py
+++ HW3/util/tasks.py
@@ -15,17 +15,18 @@ def download_titanic_dataset(**context):
 
 def pivot_dataset(**context):
     titanic_df = context['task_instance'].xcom_pull(key='download_titanic_dataset', task_ids='download_titanic_dataset')
-    #titanic_df = pd.read_csv(get_path('titanic.csv'))
-    df = titanic_df.pivot_table(index=['Sex'],
+    dt = pd.read_json(titanic_df)
+    df = dt.pivot_table(index=['Sex'],
                                 columns=['Pclass'],
                                 values='Name',
                                 aggfunc='count').reset_index()
-    #df.to_csv(get_path('titanic_pivot.csv'))
-    context['task_instance'].xcom_push(key='titanic_pivot', value=df)
+    dt = dt.to_json()
+    context['task_instance'].xcom_push(key='titanic_pivot', value=dt)
 
 def mean_fare_per_class(**context):
     titanic_df = context['task_instance'].xcom_pull(key='download_titanic_dataset', task_ids='download_titanic_dataset')
-    #titanic_df = pd.read_csv(get_path('titanic.csv'))
-    df = titanic_df.groupby('Pclass')['Fare'].mean()
-    #df.to_csv(get_path('titanic_mean_fares.csv'))
-    context['task_instance'].xcom_push(key='titanic_mean_fares', value=df)
+    df = pd.read_json(titanic_df)
+    dt = df.groupby('Pclass')['Fare'].mean()
+    dt = dt.to_json()
+    context['task_instance'].xcom_push(key='titanic_mean_fares', value=dt)
+
